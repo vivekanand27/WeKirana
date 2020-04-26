08e41b580e972fa5da6527444c74537bcd9552ec
vivekanand27 <vivekanand21@ymail.com>
2020-04-26 17:50:54 +0530

    Added Angular Material for UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+/* Angular material */
+// import { AngularMaterialModule } from './angular-material.module';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule
+    // AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
+  // schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
