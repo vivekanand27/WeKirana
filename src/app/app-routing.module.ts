00fd358c2b73e4e9efee0c25662eaac4c73d7f31
vivekanand27 <vivekanand21@ymail.com>
2020-04-27 17:46:33 +0530

    UI Created for User Registaration & Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { RegisterShopComponent} from './register-shop/register-shop.component';
-import { RegisterUserComponent} from './register-user/register-user.component';
+import { RegisterShopComponent} from './shop/register-shop/register-shop.component';
+import { RegisterUserComponent} from './user/register-user/register-user.component';
+import { LoginUserComponent} from './user/login-user/login-user.component';
 
 const routes: Routes = [
   { path: 'app-register-shop', component: RegisterShopComponent },
   { path: 'app-register-user', component: RegisterUserComponent },
+  { path: 'app-login-user', component: LoginUserComponent },
   { path: '', redirectTo: '/app-register-user', pathMatch: 'full' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,15 +10,17 @@ import { MaterialModule } from './angular-material.module';
 import { MatSliderModule } from '@angular/material/slider';
 
 import { NavMenuComponent} from './nav-menu/nav-menu.component';
-import {RegisterShopComponent} from './register-shop/register-shop.component';
-import {RegisterUserComponent} from './register-user/register-user.component';
+import {RegisterShopComponent} from './shop/register-shop/register-shop.component';
+import {RegisterUserComponent} from './user/register-user/register-user.component';
+import {LoginUserComponent} from './user/login-user/login-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     RegisterShopComponent,
-    RegisterUserComponent
+    RegisterUserComponent,
+    LoginUserComponent
   ],
   imports: [
     BrowserModule,
