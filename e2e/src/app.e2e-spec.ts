295d35aa28385c6f997ed9f5c35e8125f7a4de78
vivekanand27 <vivekanand21@ymail.com>
2020-05-18 20:29:28 +0530

    Added new feature

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to eRaasan!');
+    expect(page.getTitleText()).toEqual('Welcome to WeKirana!');
   });
 
   afterEach(async () => {
---
+++ src/app/services/register-user.service.ts
@@ -0,0 +1,8 @@
+import { User } from '../models/user.model';
+
+export class RegisterUserService {
+  private user: User;
+  addUser() {
+
+  }
+}
