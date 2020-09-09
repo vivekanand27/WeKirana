adbd6dbbe791570cb158405ed72f6a3d75a2880e
vivekanand27 <vivekanand21@ymail.com>
2020-09-09 19:53:56 +0530

    Angular version upgrade from 9 to 10

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -115,7 +115,7 @@ export class MaterialModule {
         // matIconRegistry.registerFontClassAlias('fontawesome', 'fa');
     }
 
-    static forRoot(): ModuleWithProviders {
+    static forRoot(): ModuleWithProviders<MaterialModule> {
         return {
             ngModule: MaterialModule,
             providers: [MatIconRegistry]
